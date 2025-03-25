b007058c4f73289a63669544967b30e0ad45d539
lyfar <egor.lyfar@gmail.com>
2025-03-25 17:24:22 +0800

    d

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Temporarily ignore ESLint errors during build
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig 
